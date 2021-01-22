8fd44993c663feff2ef1852ee11877fe1aa2e149
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-22 14:33:35 -0300

    cores

---
+++ js/function.js
@@ -0,0 +1,14 @@
+var engine = {
+    "cores": ['green','purple','pink','red','yellow','orange','grey','black'],
+    "hexadecimais":{
+      'green':'#02EF00',
+      'purple':'#790093',
+      'pink': '#F02A7E',
+      'red':'#E90808',
+      'yellow':'#E7D703',
+      'orange':'#F16529',
+      'grey':'#EBEBEB',
+      'black':'#141414',
+    },
+    "moedas":0
+}
