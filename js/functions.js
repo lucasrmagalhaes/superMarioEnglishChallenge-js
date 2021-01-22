9613eb5e9d3e6b5870e113596a270bc63957c57a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-22 15:38:28 -0300

    api de reconhecimento de voz

--- js/functions.js
+++ js/functions.js
@@ -48,3 +48,48 @@ function atualizaPontuacao(valor) {
   pontuacao.innerText = engine.moedas;
 }
 
+aplicarCorNaCaixa(sortearCor());
+
+// API de reconhecimento de voz.
+var btnGravador = document.getElementById("btn-responder");
+var transcricaoAudio = "";
+var respostaCorreta = "";
+
+if (window.SpeechRecognition || window.webkitSpeechRecognition) {
+  var SpeechAPI = window.SpeechRecognition || window.webkitSpeechRecognition;
+  var gravador = new SpeechAPI();
+
+  gravador.continuos = false;
+  gravador.lang = "en-US";
+
+  gravador.onstart = function(){
+    btnGravador.innerText = "Estou Ouvindo";
+    btnGravador.style.backgroundColor = "white";
+    btnGravador.style.color = "black";
+  }
+
+  gravador.onend = function() {
+    btnGravador.innerText = "Responder";
+    btnGravador.style.backgroundColor = "transparent";
+    btnGravador.style.color = "white";
+  }
+
+  gravador.onresult = function(event) {
+    transcricaoAudio = event.results[0][0].transcript.toUpperCase();
+    respostaCorreta = document.getElementById('cor-na-caixa').innerText.toUpperCase();
+
+    if(transcricaoAudio ===  respostaCorreta) {
+      atualizaPontuacao(1);
+    } else {
+      atualizaPontuacao(-1);
+    }
+    aplicarCorNaCaixa(sortearCor());
+  }
+  
+} else {
+  alert('Navegador não possui suporte.');
+}
+
+btnGravador.addEventListener('click', function(e){
+  gravador.start();
+})
